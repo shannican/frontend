1aa703d7d65a4703978810441c65012526ef901c
shannican <shannimura8683@gmail.com>
2025-02-28 15:16:42 +0530

    second

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://backend-bcuq.onrender.com/api",
   withCredentials: true,
 });
 
