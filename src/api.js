6dbe0df90af812cdb072c3e2f19491c1571ff60a
shannican <shannimura8683@gmail.com>
2025-03-01 13:14:39 +0530

    sendagain

--- src/api.js
+++ src/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
+  // baseURL: "http://localhost:9000/api",
   baseURL: "https://backend-bcuq.onrender.com/api",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default API;
+
